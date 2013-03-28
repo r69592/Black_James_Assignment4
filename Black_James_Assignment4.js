abbcf716b4e699a0b94f51d7dc07c6a186fe98cb
JamesOBlack <jamesoblack@fullsail.edu>
2013-03-28 16:33:06 -0400

    Adding to my Library.

---
+++ Black_James_Assignment4.js
@@ -0,0 +1,33 @@
+// James Black
+// SDI 1303
+// Project 4
+// js:: library
+
+var myLibrary = function() {
+  
+  // checkNumeric function.
+  
+var phoneNumber = "706-313-3502";
+
+  var checkNumeric = function(number) {
+    numericCheck = "";
+    var numberVal= /^(\d{3}){-}(\d{3})[-](\d{4})$/;
+    if (number.match(phone)) {
+        return (phonecheck = "Phone number " + number + "is correct");
+    } else {
+        return (phonecheck = "Phone number " + number + "is bad please retry (ex:123-456-7890)");
+    
+    }
+  
+  //checkName Function.
+  var checkName = function(name) {
+    
+  }
+  
+  // checkString pattern.
+  var checkString = function() {
+
+  }
+  
+ }
+}
