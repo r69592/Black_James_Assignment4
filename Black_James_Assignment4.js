df233b867752265984cfdb0451af132919415555
JamesOBlack <jamesoblack@fullsail.edu>
2013-03-28 20:07:18 -0400

    Not all working yet.

--- Black_James_Assignment4.js
+++ Black_James_Assignment4.js
@@ -5,24 +5,55 @@
 
 var myLibrary = function() {
   
+ var phoneNumber = "706-313-3502";
+ var emailAddress = "jamesoblack@fullsail.edu";
+ 
+ 
+ 
+   // checkNumeric function.
+  var checkPhone = function(number) {
+    phoneCheck = "";
+    var phone = /\d{3}-\d{3}-\d{4}/;
     if (number.match(phone)) {
-        return (phonecheck = "Phone number " + number + "is correct");
+        console.log("Phone number is correct");
     } else {
-        return (phonecheck = "Phone number " + number + "is bad please retry (ex:123-456-7890)");
-    
+        console.log("Phone number is bad please retry (ex:123-456-7890)");
     }
+  };
   
-  //checkName Function.
-  var checkName = function(name) {
-    
-  }
-  
-  // checkString pattern.
-  var checkString = function() {
+ //checkEmail Function.
+  var checkEmail = function(email) {
+    emailCheck = "";
+    var email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+/;
+    if (email.test(email)) {
+        console.log("Email address is valid.");
+    } else {
+        console.log("Email address Not valid.");
+    }
+  };
+    // Sort Object
+ var sortObject = function(array, key) {
+	var	arrKey = (a * k);
+		arrKey(4 * 8)
+		return array;
+	}
+	
+};
+ // Check URL
+var strVarifyIfURL = function (http) {
+		var reg = /^(https?):\/\/.*$/;
+		return reg.test(str); 
+		
+strVarifyIfURL(http://123.123.13.2);
 
-  }
+
+ // Check Title Case.
+  var toTitleCase = function (toTransform) {
+    return toTransform.replace(/\b([a-z])/g, function (_. initial) {
+        return initial.toUpperCase();
+        });
+
+ //
   
- }
-}
   
-console.log(checknumeric(7063133502));
+console.log(myLibrary);
