4112d066955d176c69f81999874fc5da233163f1
JamesOBlack <jamesoblack@fullsail.edu>
2013-04-03 19:30:13 -0400

    just found all the functions I need in regexp
    Hope we can use regexp in the rest of our classes.

--- Black_James_Assignment4.js
+++ Black_James_Assignment4.js
@@ -3,64 +3,113 @@
 // Project 4
 // js:: library
 
-var myLibrary = function() {
-  
- var phoneNumber = "706-313-3502";
- var emailAddress = "jamesoblack@fullsail.edu";
- 
- 
- 
-   // checkNumeric function.
-  var checkPhone = function(number) {
-    phoneCheck = "";
-    var phone = /\d{3}-\d{3}-\d{4}/;
-    if (number.match(phone)) {
-        console.log("Phone number is correct");
-    } else {
-        console.log("Phone number is bad please retry (ex:123-456-7890)");
-    }
-  };
-  
- //checkEmail Function.
-  var checkEmail = function(email) {
-    emailCheck = "";
-    var email = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+/;
-    if (email.test(email)) {
-        console.log("Email address is valid.");
-    } else {
-        console.log("Email address Not valid.");
-    }
-  };
-    // Sort Object
- var sortObject = function(array, key) {
-	var	arrKey = (a * k);
-		arrKey(4 * 8)
-		return array;
-	}
-	
-};
- // Check URL
-var strVarifyIfURL = function (http) {
+var library = {
+	// Strings
+
+	strVarifyIfPhoneNumber: function (str) {
+		var reg = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+		return reg.test(str); 
+	},
+
+	strVarifyIfEmail: function (str) {
+		var reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+		return reg.test(str); 
+	},
+
+	strVarifyIfURL: function (str) {
 		var reg = /^(https?):\/\/.*$/;
 		return reg.test(str); 
-}
+	},
+
+	strTitleCase: function (str) {
+		return str.replace(/\w\S*/g, function(txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+	},
+
+	strCsvChangeDelimiter: function (str,oldDelimiter,newDelimiter) {
+		return str.replace(/oldDelimiter/g, newDelimiter);
+	},
+
+	// Numbers
+
+	numDecimalPlaces: function (num,decPlaces) {
+		return num.toFixed(decPlaces);
+	},
+
+	numFuzzyMatch: function (num1, num2, percentage) {
+		
+		return num1;
+	},
+
+	numDatesDiff: function (d1, d2, modifier) {
+		var milliseconds = 1000;
+
+		var date1 = new Date(d1).getTime();
+		var date2 = new Date(d2).getTime();
+
+		var difference 	= Math.round(Math.abs(date1 - date2) / milliseconds);
+		var minutes 	= Math.round(difference / 60);
+		var hours 		= Math.round(minutes / 60);
+		var days 		= Math.round(hours / 24);
+
+		
+		if (modifier === 'm') {
+			return minutes;
+		} else if (modifier === 'h') {
+			return hours;
+		} else if (modifier === 'd') {
+			return days;
+		} else {
+			console.log("Please identify a modifier of either m, h, or d");
+		};	
+	},
 
 
- // Check Title Case.
-  var toTitleCase = function (toTransform) {
-    return toTransform.replace(/\b([a-z])/g, function (_. initial) {
-        return initial.toUpperCase();
-        });
+	numStringToNumber: function (str) {
+		var num = parseInt(str); 
+		if (isNaN(num)) {    	 
+			console.log('The argument must be a number in string form.'); 
+		} else {             	 
+			return num;     	 
+		};
+	}, 
 
- // Check Total Value
- var totalValue = function (array) {
+	// Arrays
+
+	arrSmallestValue: function (array,min) {
+		var arr = [];
+		
+		for (i=0,len=array.length; i < len; i++) {
+			if (array[i] >= min) {
+				arr.push([array.splice(i,1)]);
+			};
+		}
+
+		var smallest = Math.min.apply(Math, arr);
+		
+		return smallest;
+	},
+
+	arrTotalValue: function (array) {
+		
 		var total = 0;
 		for (i=0,len=array.length; i < len; i++) {
-			var num = yb.numStringToNumber(array[i]);
+			var num = library.numStringToNumber(array[i]);
 			total += num;
 		};
 		return total;
-	};
-  
-  
-console.log(myLibrary);
+	},
+
+	arrSortObject: function (array, key) {
+		return array;
+	}
+
+};
+
+
+
+var tests = function() {
+	var numarr = [4,6,8,42,24,245]
+	console.log(library.arrSmallestValue(numarr, 10));
+	
+}; 
+tests();
